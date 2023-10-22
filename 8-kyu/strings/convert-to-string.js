6c4ade2d2bc34922ca600c22e5999deb3ef35aad
Artem <artyom.profis@gmail.com>
2023-10-22 16:21:35 -0400

    color-ghosts

---
+++ 8-kyu/strings/convert-to-string.js
@@ -0,0 +1,9 @@
+/* We need a function that can transform a string into a number.
+    What ways of achieving this do you know?*/
+
+const stringToNumber = function(str){
+    return parseInt (str)
+    //return Number(str)
+    //return str * 1;
+}
+
