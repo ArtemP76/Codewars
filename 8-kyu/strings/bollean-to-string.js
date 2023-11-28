5fe239721ee002dcf56ea91d3178af2aec2337e7
Artem <artyom.profis@gmail.com>
2023-11-27 19:12:01 -0500

    first-&-last-string-character.js

---
+++ 8-kyu/strings/bollean-to-string.js
@@ -0,0 +1,13 @@
+function booleanToString(value){
+    if (value === true) {
+        return 'true';
+    } else if (value === false) {
+        return 'false'
+    }
+
+}
+
+//Implement a function which convert the given boolean value into its string representation.
+
+//function booleanToString(b){
+//  return b.toString();
